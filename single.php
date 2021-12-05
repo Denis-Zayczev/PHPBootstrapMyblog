0340e37a1205a522be6475d96664aa447549ba88
DenisZ <zayczev2012@mail.ru>
2021-12-05 18:06:01 +0500

    IncludeRequire

--- single.php
+++ single.php
@@ -1,3 +1,4 @@
+<?php include("path.php"); ?>
 <!doctype html>
 <html lang="en">
 
@@ -17,31 +18,8 @@
 </head>
 
 <body>
-  <header class="container-fluid">
-    <div class="container">
-      <div class="row">
-        <div class="col-4">
-          <h1><a href="/">My blog</a></h1>
-        </div>
-        <nav class="col-8">
-          <ul>
-            <li><a href="#">Главная</a></li>
-            <li><a href="#">О нас</a></li>
-            <li><a href="#">Услуги</a></li>
-            <li>
-              <a href="#">
-                <i class="fa fa-user"></i> Кабинет
-              </a>
-              <ul>
-                <li><a href="#">Админ панель</a></li>
-                <li><a href="#">Выход</a></li>
-              </ul>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </div>
-  </header>
+  <!-- header -->
+  <?php include("app/include/header.php"); ?>
 
   <!-- mian -->
   <div class="container">
@@ -91,67 +69,8 @@
   </div>
 
   <!-- footer -->
+  <?php include("app/include/footer.php"); ?>
 
-  <div class="footer container-fluid">
-    <div class="footer-contnet container">
-      <div class="row">
-        <div class="footer-section about col-md-4 col-12">
-          <h3 class="logo-text">Мой блог</h3>
-          <p>Мой блог - это блог сделанный с целью обучения самого себя, а так же улучшение своих навыков в веб программирование</p>
-
-          <div class="contact">
-            <span class="fas fa-phone">&nbsp; 123-456-789</span>
-            <span class="fas fa-envelope">&nbsp; info@myblog.com</span>
-          </div>
-          <div class="socials">
-            <a href=""><i class="fab fa-facebook"></i></a>
-            <a href=""><i class="fab fa-instagram"></i></a>
-            <a href=""><i class="fab fa-twitter"></i></a>
-            <a href=""><i class="fab fa-youtube"></i></a>
-          </div>
-        </div>
-
-        <div class="footer-section links col-md-4 col-12">
-          <h3>Quick Links</h3>
-          <br>
-          <ul>
-            <a href="#">
-              <li>Собыитя</li>
-            </a>
-            <a href="#">
-              <li>Команда</li>
-            </a>
-            <a href="#">
-              <li>Упражнения</li>
-            </a>
-            <a href="#">
-              <li>Галерея</li>
-            </a>
-            <a href="#">
-              <li>Что-то ещё</li>
-            </a>
-          </ul>
-        </div>
-
-        <div class="footer-section contact-form col-md-4 col-12">
-          <h3>Контакты</h3>
-          <br>
-          <form action="index.html" method="post">
-            <input type="email" name="email" class="text-input contact-input" placeholder="Your email addres...">
-            <textarea rows="4" name="message" class="text-input contact-input" placeholder="Your message..."></textarea>
-            <button type="submit" class="btn btn-big contact-btn">
-            <i class="fas fa-envelope"></i>
-            Отправить
-          </button>
-          </form>
-        </div>
-
-      </div>
-      <div class="footer-bottom">
-        &copy; myblog.com | Designed by DenisZ
-      </div>
-    </div>
-  </div>
   <!-- Optional JavaScript; choose one of the two! -->
 
   <!-- Option 1: Bootstrap Bundle with Popper -->
