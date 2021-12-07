b4c84f45a8e638f2a012070e681fb4dea91bccb8
DenisZ <zayczev2012@mail.ru>
2021-12-07 16:01:11 +0500

    07december21

--- app/database/db.php
+++ app/database/db.php
@@ -9,20 +9,76 @@ function tt($value)
   echo '</pre>';
 }
 
-function selectAll($table)
-{
-  global $pdo;
-  $sql = "SELECT * FROM $table";
-  $query = $pdo->prepare($sql);
-  $query->execute();
-  $errInfo = $query->errorInfo();
 
+
+//Проверка выполнения запросы к БД
+function dbCheckError($query){
+  $errInfo = $query->errorInfo();
   if ($errInfo[0] !== PDO::ERR_NONE) {
     echo $errInfo[2];
     exit();
   }
+  return true;
+}
+
+// Запрос на получения данных с одньй таблице
+function selectAll($table, $params = [])
+{
+
+  $sql = "SELECT * FROM $table";
+  global $pdo;
+  if(!empty($params)){
+    $i = 0;
+    foreach ($params as $key => $value){
+      if (!is_numeric($value)){
+        $value = "'".$value."'";
+      }
+      if ($i === 0){
+        $sql = $sql . " WHERE $key = $value";
+      }else {
+        $sql = $sql . " AND $key = $value";
+      }
+      $i++;
+    }
+  }
+
+  $query = $pdo->prepare($sql);
+  $query->execute();
+  dbCheckError($query);
 
   return $query->fetchAll();
 }
 
-tt(selectAll('users'));
+// Запрос на получения одной строки c выбранной таблицы
+function selectOne($table, $params = [])
+{
+  $sql = "SELECT * FROM $table";
+  global $pdo;
+  if(!empty($params)){
+    $i = 0;
+    foreach ($params as $key => $value){
+      if (!is_numeric($value)){
+        $value = "'".$value."'";
+      }
+      if ($i === 0){
+        $sql = $sql . " WHERE $key = $value";
+      }else {
+        $sql = $sql . " AND $key = $value";
+      }
+      $i++;
+    }
+  }
+  // $sql = $sql . " LIMIT 1";
+  $query = $pdo->prepare($sql);
+  $query->execute();
+  dbCheckError($query);
+  return $query->fetch();
+}
+
+$params = [
+  'admin' => 1,
+  'username' => 'some'
+];
+
+// tt(selectAll('users' , $params));
+tt(selectOne('users'));
